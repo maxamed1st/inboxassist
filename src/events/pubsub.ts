33e877c9a69f6d2a1d6f3b843601dbab28a5e756
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-10-09 22:10:25 +0000

    set up pubsub

---
+++ src/events/pubsub.ts
@@ -0,0 +1,27 @@
+import { redisClient } from "./client.js";
+import type { Channels, MessagePayloads } from "./types.js";
+
+// Publisher
+export const publish = async <T extends Channels>(
+  channel: T,
+  message: MessagePayloads[T]
+) => {
+  await redisClient.publish(channel, JSON.stringify(message));
+  console.log(`Published to ${channel}:`, message);
+};
+
+// Subscriber
+export const subscribe = async <T extends Channels>(
+  channel: T,
+  callback: (message: MessagePayloads[T]) => void
+) => {
+  const subscriber = redisClient.duplicate();
+  await subscriber.connect();
+
+  await subscriber.subscribe(channel, (message) => {
+    const parsed: MessagePayloads[T] = JSON.parse(message);
+    callback(parsed);
+  });
+
+  console.log(`Subscribed to ${channel}`);
+};
