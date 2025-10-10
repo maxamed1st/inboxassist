75034cb9e93b7e7972cc610e112559a93f4f3d1d
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-10-10 07:44:02 +0000

    fix: typo

--- src/events/types.ts
+++ src/events/types.ts
@@ -1,7 +1,7 @@
 export type Channels = 
   "message:user" | "message:assistant" | "message:system" | "message:prune" |
   "action:compose" | "action:send" | "action:edit" | "action:move" |
-  "email:new" | "email:summerised" | "email:sent" | "email:moved" |
+  "email:new" | "email:summarised" | "email:sent" | "email:moved" |
   "email:composed" | "email:edited" |
   "email:login" | "email:logout" | "email:prune" |
   "billing:balance" | "billing:buy";
