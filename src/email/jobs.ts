caad61515257f998a399699ee8d8d836aad3c5eb
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-10-16 19:10:06 +0000

    create a job to refresh access tokens

---
+++ src/email/jobs.ts
@@ -0,0 +1,30 @@
+import { googleOauth2Client } from "./clients.js";
+import { getAccountByProviderAccountId, updateAccount } from "./queries.js";
+import jwt from "jsonwebtoken";
+
+// refresh access token when expired
+export async function refreshGmailTokens(accountId: string) {
+  const account = await getAccountByProviderAccountId(accountId);
+  if (!account?.refreshToken) {
+    throw new Error("No refresh token found for account");
+  }
+
+  googleOauth2Client.setCredentials({ refresh_token: account?.refreshToken });
+  const { credentials } = await googleOauth2Client.refreshAccessToken();
+  if (!credentials || !credentials.access_token || !credentials.refresh_token || !credentials.id_token || !credentials.expiry_date) {
+    throw new Error("Failed to refresh access token");
+  }
+  
+  // decode the user's email from the ID token
+  const decoded = jwt.decode(credentials.id_token) as { email: string };
+  const providerAccountId = decoded?.email ?? "unknown";
+
+  const values = {
+    accessToken: credentials.access_token,
+    refreshToken: credentials.refresh_token,
+    expiresAt: Math.floor(credentials.expiry_date / 1000),
+    updatedAt: Math.floor(Date.now() / 1000),
+  };
+
+  await updateAccount(providerAccountId, values);
+}
--- src/email/queries.ts
+++ src/email/queries.ts
@@ -1,5 +1,6 @@
 import { db } from "@/db/client.js";
 import { accountsTable } from "@/db/schema.js";
+import { eq } from "drizzle-orm";
 
 export async function insertAccount(values: typeof accountsTable.$inferInsert) {
   return db
@@ -9,4 +10,24 @@ export async function insertAccount(values: typeof accountsTable.$inferInsert) {
     }
     )
     .returning();
-}
+  }
+
+export async function updateAccount(providerAccountId: string, values: Partial<typeof accountsTable.$inferInsert>) {
+  return db
+    .update(accountsTable)
+    .set({
+        ...values,
+    }
+    )
+    .where(eq(accountsTable.providerAccountId, providerAccountId))
+    .returning();
+  }
+
+export async function getAccountByProviderAccountId(providerAccountId: string) {
+  return db
+    .select()
+    .from(accountsTable)
+    .where(eq(accountsTable.providerAccountId, providerAccountId))
+    .limit(1)
+    .then((rows) => rows[0]);
+  }
