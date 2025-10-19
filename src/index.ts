855a41447287f1290eb8050a028d8e12a1755d5e
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-10-19 21:02:32 +0000

    initialize email module

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import initializeRedis from "@/events/index";
 import initializeServer from "@/server";
+import initializeMail from "@/email/index";
 
 async function main() {
   // initializations
   await initializeRedis();
   initializeServer();
+  await initializeMail();
 }
 
 main()
