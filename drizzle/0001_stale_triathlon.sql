aae67e3539dbce4447f88edaf7ebc50e7c01dd75
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-11-05 02:02:05 +0000

    generate migration for db change

---
+++ drizzle/0001_stale_triathlon.sql
@@ -0,0 +1,14 @@
+ALTER TABLE "accounts" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "accounts" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "actions" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "actions" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "connections" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "connections" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "emails" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "emails" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "messages" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "messages" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;
