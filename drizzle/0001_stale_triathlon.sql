699369e610b158f00d8eed43897b9f522554484d
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-11-05 02:16:48 +0000

    handle altering integer columns

--- drizzle/0001_stale_triathlon.sql
+++ drizzle/0001_stale_triathlon.sql
@@ -1,14 +1,14 @@
-ALTER TABLE "accounts" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "accounts" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "actions" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "actions" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "connections" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "connections" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "emails" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "emails" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "messages" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "messages" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "subscriptions" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "subscriptions" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "createdAt" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "users" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp;
+ALTER TABLE "accounts" ALTER COLUMN "createdAt" SET DATA TYPE timestamp USING to_timestamp("createdAt" / 1000);--> statement-breakpoint
+ALTER TABLE "accounts" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp USING to_timestamp("updatedAt" / 1000);--> statement-breakpoint
+ALTER TABLE "actions" ALTER COLUMN "createdAt" SET DATA TYPE timestamp USING to_timestamp("createdAt" / 1000);--> statement-breakpoint
+ALTER TABLE "actions" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp USING to_timestamp("updatedAt" / 1000);--> statement-breakpoint
+ALTER TABLE "connections" ALTER COLUMN "createdAt" SET DATA TYPE timestamp USING to_timestamp("createdAt" / 1000);--> statement-breakpoint
+ALTER TABLE "connections" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp USING to_timestamp("updatedAt" / 1000);--> statement-breakpoint
+ALTER TABLE "emails" ALTER COLUMN "createdAt" SET DATA TYPE timestamp USING to_timestamp("createdAt" / 1000);--> statement-breakpoint
+ALTER TABLE "emails" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp USING to_timestamp("updatedAt" / 1000);--> statement-breakpoint
+ALTER TABLE "messages" ALTER COLUMN "createdAt" SET DATA TYPE timestamp USING to_timestamp("createdAt" / 1000);--> statement-breakpoint
+ALTER TABLE "messages" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp USING to_timestamp("updatedAt" / 1000);--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "createdAt" SET DATA TYPE timestamp USING to_timestamp("createdAt" / 1000);--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp USING to_timestamp("updatedAt" / 1000);--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "createdAt" SET DATA TYPE timestamp USING to_timestamp("createdAt" / 1000);--> statement-breakpoint
+ALTER TABLE "users" ALTER COLUMN "updatedAt" SET DATA TYPE timestamp USING to_timestamp("updatedAt" / 1000);
