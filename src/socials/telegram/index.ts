51d0a9616d2682ca290c730b17804336d7806653
Mohamed <mohha783@ad.liu.se>
2025-11-07 21:33:18 +0100

    access telegram bot client locally

--- src/socials/telegram/index.ts
+++ src/socials/telegram/index.ts
@@ -1,19 +1,17 @@
 import { sendMessage } from "@/socials/telegram/utils/sendMessage"
 import { subscribe } from "@/events/broker";
 import handleCommands from "./utils/commands";
-import { telegramBotClient } from "./client";
 
 export default async function main() {
-  const bot = telegramBotClient();
   // handle commands
   await handleCommands();
 
   // register listeners
   subscribe("message:system", "telegram_bot", async ({id, content}) => {
-    sendMessage(bot, id, content);
+    sendMessage(id, content);
   })
   
   subscribe("message:assistant", "telegram_bot", async ({id, content}) => {
-    sendMessage(bot, id, content);
+    sendMessage(id, content);
   })
 }
--- src/socials/telegram/utils/sendMessage.ts
+++ src/socials/telegram/utils/sendMessage.ts
@@ -1,17 +1,17 @@
 import { getTelegramUserId } from "@/db/queries/connections";
-import { Context, Telegraf } from "telegraf";
-import { Update } from "telegraf/types";
+import { telegramBotClient } from "../client";
 
-export async function sendMessage(bot: Telegraf<Context<Update>>, id: string, content: string) {
-    const telegramUser = await getTelegramUserId(id)
-    if (!telegramUser) {
-      console.error("Could not find Telegram connection for:", id);
-      return;
-    }
-    bot.telegram.sendMessage(telegramUser.id, content, { parse_mode: "HTML"}).catch(() => {
-      // send message without html parsing
-      bot.telegram.sendMessage(telegramUser.id, content).catch( err => {
-        console.error("Failed to send message to telegram user", err);
-      })
-    });
+export async function sendMessage(id: string, content: string) {
+  const bot = telegramBotClient();
+  const telegramUser = await getTelegramUserId(id)
+  if (!telegramUser) {
+    console.error("Could not find Telegram connection for:", id);
+    return;
+  }
+  bot.telegram.sendMessage(telegramUser.id, content, { parse_mode: "HTML"}).catch(() => {
+    // send message without html parsing
+    bot.telegram.sendMessage(telegramUser.id, content).catch( err => {
+      console.error("Failed to send message to telegram user", err);
+    })
+  });
 }
