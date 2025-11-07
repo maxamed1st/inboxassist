50ea8d1d6076c7ee8bf27e4905897702b3ed4a28
Mohamed <mohha783@ad.liu.se>
2025-11-07 21:32:12 +0100

    extract commands into a file

--- src/socials/telegram/index.ts
+++ src/socials/telegram/index.ts
@@ -1,26 +1,14 @@
-import { telegramBotClient } from "@/socials/telegram/client";
-import { connectEmail, initializeUser } from "@/socials/telegram/utils/listners";
 import { sendMessage } from "@/socials/telegram/utils/sendMessage"
 import { subscribe } from "@/events/broker";
+import handleCommands from "./utils/commands";
+import { telegramBotClient } from "./client";
 
 export default async function main() {
   const bot = telegramBotClient();
-  bot.start(async (ctx) => {
-    const user = await initializeUser(String(ctx.chat.id));
+  // handle commands
+  await handleCommands();
 
-    switch(user.isNewUser) {
-      case null:
-        ctx.reply("Welcome to inboxassit - Your frictionless email assistant. Something went wrong with initializing your assistant. Please try again. \
-          \n You can also use /connect to authorize your email.");
-        break;
-      default:
-        ctx.reply("Welcome to inboxassist - Your frictionless email assist.  \n Run /connect to start managing your emails");
-        break;
-    }
-  });
-
-  bot.command("connect", async (ctx) => connectEmail(String(ctx.chat.id)));
-
+  // register listeners
   subscribe("message:system", "telegram_bot", async ({id, content}) => {
     sendMessage(bot, id, content);
   })
---
+++ src/socials/telegram/utils/commands.ts
@@ -0,0 +1,21 @@
+import { telegramBotClient } from "@/socials/telegram/client";
+import { connectEmail, initializeUser } from "@/socials/telegram/utils/listners";
+
+export default async function handleCommands() {
+  const bot = telegramBotClient();
+  bot.start(async (ctx) => {
+    const user = await initializeUser(String(ctx.chat.id));
+
+    switch(user.isNewUser) {
+      case null:
+        ctx.reply("Welcome to inboxassit - Your frictionless email assistant. Something went wrong with initializing your assistant. Please try again. \
+          \n You can also use /connect to authorize your email.");
+        break;
+      default:
+        ctx.reply("Welcome to inboxassist - Your frictionless email assist.  \n Run /connect to start managing your emails");
+        break;
+    }
+  });
+
+  bot.command("connect", async (ctx) => connectEmail(String(ctx.chat.id)));
+}
