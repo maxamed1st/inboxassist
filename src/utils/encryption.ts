65371c2394b11ebc010e6e33daab00e77eb0363f
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-11-08 22:36:10 +0000

    create helpers for encrypting data

---
+++ src/utils/encryption.ts
@@ -0,0 +1,63 @@
+import crypto from "crypto"
+
+// get secret key
+export function getEncryptionKey() {
+  const key = process.env.ENCRYPTION_KEY;
+
+  if(!key) {
+    throw new Error("Missing encryption key");
+  }
+
+  return Buffer.from(key, "hex");
+}
+
+// Encrypt data
+export function encrypt(text: string, key: Buffer) {
+  // Generate a random initialization vector
+  const iv = crypto.randomBytes(16);
+  
+  // Create cipher
+  const cipher = crypto.createCipheriv('aes-256-gcm', key, iv);
+  
+  // Encrypt the data
+  let encrypted = cipher.update(text, 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  
+  // Get the auth tag for authentication
+  const authTag = cipher.getAuthTag();
+  
+  // Return iv, encrypted data, and auth tag
+  const encryptedObject = {
+    iv: iv.toString('hex'),
+    encryptedData: encrypted,
+    authTag: authTag.toString('hex')
+  };
+
+  return `${encryptedObject.iv}:${encryptedObject.authTag}:${encryptedObject.encryptedData}`
+}
+
+// Decrypt data
+export function decrypt(encryptedString: string, key: Buffer) {
+  const parts = encryptedString.split(":")
+  const [ iv, authTag, encryptedData ] = parts;
+
+  if (!iv || !authTag || !encryptedData) {
+    throw new Error("Wrong format for encrypted data");
+  }
+
+  // Create decipher
+  const decipher = crypto.createDecipheriv(
+    'aes-256-gcm',
+    key,
+    Buffer.from(iv, 'hex')
+  );
+  
+  // Set the auth tag
+  decipher.setAuthTag(Buffer.from(authTag, 'hex'));
+  
+  // Decrypt the data
+  let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+  decrypted += decipher.final('utf8');
+  
+  return decrypted;
+}
