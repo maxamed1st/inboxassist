9f8f2ba894cec295a1584acb46354741f78a86c1
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-11-16 08:00:01 +0000

    get previouse messages

--- src/db/queries/messages.ts
+++ src/db/queries/messages.ts
@@ -1,4 +1,4 @@
-import { and, eq } from "drizzle-orm";
+import { and, eq, or, sql } from "drizzle-orm";
 import { db } from "../client";
 import { messagesTable } from "../schema";
 
@@ -45,6 +45,24 @@ export async function getMessageById(messageId: string) {
       .from(messagesTable)
       .where(eq(messagesTable.id, messageId))
       .limit(1)
+
+    return res;
+  } catch(err) {
+    console.error("Failed to get message by id", err)
+    return null
+  }
+}
+
+export async function getPreviouseMessages(threadId: string) {
+  try{
+    const res = await db
+      .select({ content: messagesTable.content, role: messagesTable.role })
+      .from(messagesTable)
+      .where(
+        or(
+          eq(messagesTable.id, threadId),
+          sql`${messagesTable.references}->>0 = ${threadId}`
+        ));
 
     return res;
   } catch(err) {
