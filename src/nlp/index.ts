1d3c92c6b67fcb6e4779bff3f2cae76e47c68c92
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-11-23 17:22:06 +0000

    register generic gpt listner for unknonw actions

--- src/nlp/index.ts
+++ src/nlp/index.ts
@@ -2,10 +2,12 @@ import { subscribe } from "@/events/broker";
 import { summerizeEmail } from "@/nlp/listners/summeriser";
 import { classifyUserIntent } from "@/nlp/listners/classifier";
 import { composeEmail } from "./listners/composer";
+import { genericGPT } from "./listners/generic";
 
 export default async function main() {
   // register subscripers
   subscribe("email:new", "nlp", summerizeEmail)
   subscribe("message:user", "nlp", classifyUserIntent)
   subscribe("action:compose", "nlp", composeEmail)
+  subscribe("action:unknown", "nlp", genericGPT);
 }
