43917bba1352235bcfdb31a6cbbabf2437a4b7e4
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-11-24 17:11:34 +0000

    set imap uid as none null

---
+++ drizzle/0011_volatile_hitman.sql
@@ -0,0 +1,1 @@
+ALTER TABLE "emails" ALTER COLUMN "imapUid" SET NOT NULL;
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -49,7 +49,7 @@ export const emailsTable = pgTable("emails", {
     .notNull()
     .references(() => accountsTable.id, { onDelete: "cascade" }),
   externalEmailId: varchar({ length: 255 }).notNull(),
-  imapUid:  integer(),
+  imapUid:  integer().notNull(),
   inReplyTo: varchar({ length: 255 }),
   references: jsonb().default(sql`'[]'::jsonb`).notNull().$type<string[]>(),
   subject: varchar({ length: 998 }).notNull(),
