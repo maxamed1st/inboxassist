578198aeb519889d3dda2d679ddca9b7ebd23c5d
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-11-24 16:24:39 +0000

    use back slash for flags

--- src/email/listners/commands.ts
+++ src/email/listners/commands.ts
@@ -95,7 +95,7 @@ export async function toggleEmailReadStatus({userId, emailId, threadId }: { user
     const isSeen = message.flags?.has("//seen");
 
     if(isSeen) {
-      await client.messageFlagsRemove(message.uid, ["//seen"]);
+      await client.messageFlagsRemove(message.uid, ["\\seen"]);
       await publish("message:assistant", { 
         id: userId,
         content: "The email has been marked read",
@@ -104,7 +104,7 @@ export async function toggleEmailReadStatus({userId, emailId, threadId }: { user
       });
     }
     else {
-      await client.messageFlagsAdd(message.uid, ["//seen"]);
+      await client.messageFlagsAdd(message.uid, ["\\seen"]);
       await publish("message:assistant", { 
         id: userId,
         content: "The email has been marked unread",
