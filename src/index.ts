26c7c8634ed29184a1c37d5d058fbc9a79a6b713
Mohamed <mohha783@ad.liu.se>
2025-12-03 18:23:50 +0100

    initialize billing module

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import initializeServer from "@/server";
 import initializeMail from "@/email/index";
 import initializeTelegramBot from "@/socials/telegram/index";
 import initializeNLP from "@/nlp/index"
+import initializeBilling from "@/billing/index";
 
 async function main() {
   // initializations
   await initializeDb();
   await initializeRedis();
   initializeServer();
+  initializeBilling();
   await initializeMail();
   initializeTelegramBot();
   initializeNLP();
