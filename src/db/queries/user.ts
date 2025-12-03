44830e5d95fbcb3000790f86679da666dceb4e2b
Mohamed <mohha783@ad.liu.se>
2025-12-03 18:31:42 +0100

    update user in db

--- src/db/queries/user.ts
+++ src/db/queries/user.ts
@@ -1,5 +1,6 @@
 import { usersTable } from "../schema";
 import { db } from "../client";
+import { eq } from "drizzle-orm";
 
 export async function insertUser(values: typeof usersTable.$inferInsert) {
   try{
@@ -13,6 +14,23 @@ export async function insertUser(values: typeof usersTable.$inferInsert) {
     return res;
   } catch(err) {
     console.error("Failed to insert User:", err)
+    return null;
+  }
+}
+
+export async function updateUserById(userId: string, values: Partial<typeof usersTable.$inferInsert>) {
+  try{
+    const [ res ] = await db
+      .update(usersTable)
+      .set({
+          ...values,
+      })
+      .where(eq(usersTable.id, userId))
+      .returning();
+    
+    return res;
+  } catch(err) {
+    console.error("Failed to update User:", err)
     return null;
   }
 }
