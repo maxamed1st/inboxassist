9c98e17cab8098b4987c6405668b2d71bca642bd
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-12-12 05:41:00 +0000

    move gmail callback into handlers folder

--- src/email/api/routes.ts
+++ src/email/api/routes.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { gmailCallback } from "@/email/api/handlers";
+import { gmailCallback } from "@/email/api/handlers/gmail";
 
 const router = express.Router();
 
