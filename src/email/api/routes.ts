f9a821044910a12b17eabdb187483dfcecde9a4d
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2025-12-13 13:13:29 +0000

    register microsoft route

--- src/email/api/routes.ts
+++ src/email/api/routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { gmailCallback } from "@/email/api/handlers/gmail";
+import { microsftCallback } from "./handlers/microsoft";
 
 const router = express.Router();
 
 router.get("/gmail/callback", gmailCallback);
+router.get("/microsoft/callback", microsftCallback);
 
 export default router;
