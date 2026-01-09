045fdec3c956a4ea5a9c5cf18e7bd24dc6e9f14f
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-01-09 18:38:53 +0000

    pass raw request to routes

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,6 @@ export default function server() {
     const app = express();
     const PORT = process.env.PORT || 3000;
 
-    app.use(express.json());
     app.use(authRouter);
     app.use(webhookRouter);
 
