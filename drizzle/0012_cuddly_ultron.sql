758e7071c8feb346829077ddbeefa72adb94f4b5
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-01-09 19:34:51 +0000

    fix: convert current integer dates to timestamp

--- drizzle/0012_cuddly_ultron.sql
+++ drizzle/0012_cuddly_ultron.sql
@@ -1,3 +1,3 @@
-ALTER TABLE "subscriptions" ALTER COLUMN "periodStart" SET DATA TYPE timestamp;--> statement-breakpoint
-ALTER TABLE "subscriptions" ALTER COLUMN "periodEnd" SET DATA TYPE timestamp;--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "periodStart" SET DATA TYPE timestamp USING to_timestamp("periodStart");--> statement-breakpoint
+ALTER TABLE "subscriptions" ALTER COLUMN "periodEnd" SET DATA TYPE timestamp USING to_timestamp("periodEnd");--> statement-breakpoint
 ALTER TABLE "users" ALTER COLUMN "subscriptionId" SET DATA TYPE varchar(255);
