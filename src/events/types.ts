0f4475241cdafc8c66a1b202d9626ab716d2327a
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-01-18 06:36:06 +0000

    specify events that are not used in this version

--- src/events/types.ts
+++ src/events/types.ts
@@ -22,11 +22,11 @@ export interface MessagePayloads {
 
   // email events
   "email:new": { userId: string, emailId: string };
-  "email:summarised": { id: string, content: string };
+  "email:summarised": { id: string, content: string }; // not implemented
   "email:composed": { id: string, content: string, to?: string, inReplyToId?: string, threadId?: string };
   "email:edited": { id: string, content: string };
-  "email:sent": { id: string };
-  "email:moved": { id: string };
+  "email:sent": { id: string }; // not implemented
+  "email:moved": { id: string }; // not implemented
   "email:toggleReadStatus": { userId: string, emailId: string, threadId?: string }
   "email:connect": { userId: string, platform: string };
   "email:disconnect": { userId: string };
