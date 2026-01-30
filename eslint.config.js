5aaad8bfa4197ddabfa4852ed460456e506c0805
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-01-30 16:29:41 +0000

    chore: turn off rule handled by eslint

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,7 @@ export default defineConfig([
     languageOptions: { globals: globals.node },
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   },
 ]);
