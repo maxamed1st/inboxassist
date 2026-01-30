da15d278f826644f3acacb5f0115e06cda61ca1f
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-01-30 16:17:36 +0000

    feat: add stylistic ta manage format with eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,15 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
+import stylistic from "@stylistic/eslint-plugin";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
+    plugins: {
+      js,
+      "@stylistic": stylistic
+    },
     extends: ["js/recommended"],
     languageOptions: { globals: globals.node }
   },
