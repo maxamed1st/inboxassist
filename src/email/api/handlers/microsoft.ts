7a0f0e497698b3ae1bde6432fa52965fe1b219e1
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-02-06 07:24:07 +0000

    chore(tokenRefresh): remove redundant check

--- src/email/api/handlers/microsoft.ts
+++ src/email/api/handlers/microsoft.ts
@@ -14,10 +14,7 @@ function partitionTokenCache(fullcache: string, homeAccountId: string) {
     if(!Array.isArray(cache[key])) {
       cache[key] = [];
     }
-
-    if(cache[key]) {
-      cache[key] = cache[key].filter( (t: any) => t.homeAccountId == homeAccountId);
-    }
+    cache[key] = cache[key].filter( (t: any) => t.homeAccountId == homeAccountId);
   }
 
   return JSON.stringify(cache);
