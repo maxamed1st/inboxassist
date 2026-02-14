3021dcc50416d226e5eb4096fdb7fe277a7d8c0d
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-02-14 03:21:56 +0000

    test: mock telegraf module to fix networking issues

--- tests/utils/mocks.ts
+++ tests/utils/mocks.ts
@@ -1,6 +1,5 @@
 import { vi } from "vitest";
 import { nlpClient } from "@/nlp/clients";
-import { bot } from "@/socials/telegram/clients";
 
 export function mockNLPClient() {
   const nlpMock = vi.fn().mockResolvedValue({
@@ -11,15 +10,24 @@ export function mockNLPClient() {
 }
 
 export function mockBot() {
-  const now = new Date();
-
-  const sendMessageMock = vi.fn().mockResolvedValue({
-    message_id: 22,
-    chat: { id: 2 },
-    text: "mocked Message",
-    date: now,
-  });
-
-  vi.spyOn(bot.telegram, "sendMessage").mockImplementation(sendMessageMock);
-  vi.spyOn(bot, "launch")
+  vi.mock("telegraf", () => ({
+    Telegraf: function() {
+      return {
+        launch: vi.fn().mockResolvedValue(undefined),
+        stop: vi.fn().mockResolvedValue(undefined),
+        on: vi.fn().mockReturnThis(),
+        command: vi.fn().mockReturnThis(),
+        start: vi.fn().mockReturnThis(),
+        telegram: {
+          sendMessage: vi.fn().mockResolvedValue({
+            message_id: 22,
+            chat: { id: 2 },
+            text: "mocked Message",
+            date: new Date(),
+          }),
+          getMe: vi.fn()
+        },
+      }
+    }
+  }))
 }
