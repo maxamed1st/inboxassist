518d8515fc927d6b03f3b01ce7fa2ca1ce819375
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-08-02 21:47:28 +0200

    refactor(err): throw inline errors for billing events

--- src/billing/listners.ts
+++ src/billing/listners.ts
@@ -2,81 +2,72 @@ import { stripe } from "@/billing/clients";
 import { getSubscriptionByUserId } from "@/db/queries/billing";
 import { getUserById } from "@/db/queries/user";
 import { publish } from "@/events/broker";
+import { ctxError } from "@/utils/errorHandling";
 
 export async function checkout({ userId }: { userId: string }) {
-  try {
-    const user = await getUserById(userId);
+  const user = await getUserById(userId);
 
-    if(!user) throw new Error (`Failed to get user: ${userId}`)
-
-    if (user.subscriptionStatus === "active") {
-      await publish("message:system", {
-        userId,
-        content: `You already have a subscription. You can manage it with /manage_subscription.`
-      });
-      return;
-    }
-
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
-          price: process.env.STRIPE_PRICE_ID,
-          quantity: 1,
-        },
-      ],
-      mode: 'subscription',
-      success_url: process.env.BOT_URL,
-      cancel_url: process.env.BOT_URL,
-      subscription_data: {
-        metadata: { userId },
-      },
-      automatic_tax: {
-        enabled: true
-      },
-      tax_id_collection: {
-        enabled: true
-      }
-    });
-
-    if(!session.url) {
-      throw new Error(`redirect url missing`)
-    }
-
-    await publish("message:system", {
-      userId: userId,
-      content: `[checkout](${session.url})`
-    });
-
-  } catch (error) {
-    console.error("Failed to create checkout session", error);
-    throw error
+  if (!user) {
+    throw ctxError("checkout: Failed to get user", { ctx: { userId } })
   }
+
+  if (user.subscriptionStatus === "active") {
+    await publish("message:system", {
+      userId,
+      content: `You already have a subscription. You can manage it with /manage_subscription.`
+    });
+    return;
+  }
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price: process.env.STRIPE_PRICE_ID,
+        quantity: 1,
+      },
+    ],
+    mode: 'subscription',
+    success_url: process.env.BOT_URL,
+    cancel_url: process.env.BOT_URL,
+    subscription_data: {
+      metadata: { userId },
+    },
+    automatic_tax: {
+      enabled: true
+    },
+    tax_id_collection: {
+      enabled: true
+    }
+  });
+
+  if (!session.url) {
+    throw ctxError("checkout: redirect url missing", { ctx: { userId } })
+  }
+
+  await publish("message:system", {
+    userId: userId,
+    content: `[checkout](${session.url})`
+  });
 }
 
 export async function customerPortal({ userId }: { userId: string }) {
-  try {
-    const subscription = await getSubscriptionByUserId(userId);
+  const subscription = await getSubscriptionByUserId(userId);
 
-    if (!subscription) {
-      throw new Error(`user doesn't exist: ${userId}`);
-    }
-
-    const session = await stripe.billingPortal.sessions.create({
-      customer: subscription.providerCustomerId,
-      return_url: process.env.BOT_URL,
-    });
-
-    if(!session.url) {
-      throw new Error(`redirect url missing`)
-    }
-
-    await publish("message:system", {
-      userId: userId,
-      content: `[Manage Subscription](${session.url})`
-    });
-
-  } catch (error) {
-    console.error("Failed to create customer portal", error);
-    throw error
+  if (!subscription) {
+    throw ctxError("customerPortal: user doesn't exist", { ctx: { userId } });
   }
+
+  const session = await stripe.billingPortal.sessions.create({
+    customer: subscription.providerCustomerId,
+    return_url: process.env.BOT_URL,
+  });
+
+  if (!session.url) {
+    throw ctxError("customerPortal: redirect url missing", { ctx: { userId } })
+  }
+
+  await publish("message:system", {
+    userId: userId,
+    content: `[Manage Subscription](${session.url})`
+  });
 }
