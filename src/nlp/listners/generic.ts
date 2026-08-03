f078534496c1baec4dd6552c5dfaa85c5fea8cb4
Mohamed Hassan Ahmed <maxamed1st@gmail.com>
2026-08-03 22:02:25 +0200

    refactor(err): allow generic nlp errors to bubble and use ctxError

--- src/nlp/listners/generic.ts
+++ src/nlp/listners/generic.ts
@@ -2,33 +2,30 @@ import { publish } from "@/events/broker";
 import { nlpClient } from "@/nlp/clients";
 import { generic } from "@/nlp/utils/systemPrompt";
 import { buildContext } from "../utils/context";
+import { ctxError } from "@/utils/errorHandling";
 
 export async function genericGPT({ userId, emailId, userMessage, threadId }: { userId: string, emailId?: string, userMessage: string, threadId?: string }) {
-  try {
-    const { messages } = await buildContext({
-      userId,
-      ctx: { type: "generic", systemPrompt: generic },
-      userMessage,
-      emailId,
-      threadId
-      });
-    
-    const response = await nlpClient.chat.completions.create({
-      model: "claude-haiku-4-5-20251001",
-      store: false,
-      messages: messages,
-      temperature: 0.7,
-      max_tokens: 250
-    });
+  const { messages } = await buildContext({
+    userId,
+    ctx: { type: "generic", systemPrompt: generic },
+    userMessage,
+    emailId,
+    threadId
+  });
+
+  const response = await nlpClient.chat.completions.create({
+    model: "claude-haiku-4-5-20251001",
+    store: false,
+    messages: messages,
+    temperature: 0.7,
+    max_tokens: 250
+  });
 
   const message = response.choices[0]?.message.content;
 
-  if(!message) {
-    throw new Error("Failed to get generic response from nlp client");
+  if (!message) {
+    throw ctxError("nlpgeneric: Failed to get response from nlp client", { ctx: { userId } });
   }
 
   publish("message:assistant", { userId: userId, emailId, content: message })
-  } catch(err) {
-    throw new Error(`Failed to respond to a generic query from ${userId}: ${err}`)
-  }
 }
